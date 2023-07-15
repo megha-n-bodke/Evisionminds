51585f3b2919f91069a086ebecbe2a12d6b6f82a
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-07-15 23:35:31 +0300

    Fix linters

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-md bg-light">
-      <div className="container-fluid">
+      <div className="container-fluid cFluid">
         <img src={logo} alt="logo" />
         <button
           className="navbar-toggler"
@@ -26,23 +26,23 @@ const Navbar = () => {
           <span className="navbar-toggler-icon" />
         </button>
         <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
-          <ul className="navbar-nav my-3">
-            <li className="nav-item py-4 p-md-2 px-4">
+          <ul className="navbar-nav my-3 px-lg-5">
+            <li className="nav-item py-4 p-md-2 px-lg-4 px-4">
               <a className="nav-link active" aria-current="page" href="#home">HOME</a>
             </li>
-            <li className="nav-item py-4 p-md-2 px-4">
+            <li className="nav-item py-4 p-md-2 px-lg-4 px-4">
               <a className="nav-link active" aria-current="page" href="#about">ABOUT</a>
             </li>
-            <li className="nav-item py-4 p-md-2 px-4">
+            <li className="nav-item py-4 p-md-2 px-lg-4 px-4">
               <a className="nav-link active" aria-current="page" href="#services">SERVICES</a>
             </li>
-            <li className="nav-item py-4 p-md-2 px-4">
+            <li className="nav-item py-4 p-md-2 px-lg-4 px-4">
               <a className="nav-link active" aria-current="page" href="#solutions">SOLUTIONS</a>
             </li>
-            <li className="nav-item py-4 p-md-2 px-4">
+            <li className="nav-item py-4 p-md-2 px-lg-4 px-4">
               <a className="nav-link active" aria-current="page" href="#products">PRODUCTS</a>
             </li>
-            <li className="nav-item py-4 p-md-2 px-4">
+            <li className="nav-item py-4 p-md-2 px-lg-4 px-4">
               <a className="nav-link active" id="contact" aria-current="page" href="#contact">CONTACT</a>
             </li>
           </ul>
